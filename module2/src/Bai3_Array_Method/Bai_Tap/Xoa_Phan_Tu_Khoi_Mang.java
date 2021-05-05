83da3a47703b6046f68153b056e61fcddff4a328
thanhbinhkd95 <thanhbinhkd95@gmail.com>
2021-05-05 15:27:38 +0700

    bt

--- module2/src/Bai3_Array_Method/Bai_Tap/Xoa_Phan_Tu_Khoi_Mang.java
+++ module2/src/Bai3_Array_Method/Bai_Tap/Xoa_Phan_Tu_Khoi_Mang.java
@@ -4,22 +4,31 @@ import java.util.Scanner;
 
 public class Xoa_Phan_Tu_Khoi_Mang {
     public static void main(String[] args) {
-        int[] array = {10,4,6,7,8,6,0,0,0,0};
-        System.out.println("Mảng trước khi xóa: "+ "10,4,6,7,8,6,0,0,0,0");
+        /*Tạo mảng*/
+        int[] array = {100, 43, 16, 27, 8, 6, 7, 11, 22, 33};
+        System.out.println("Mảng trước khi xóa");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(" " + array[i]);
+        }
+        ;
+        System.out.println();
+
+        /*Tạo lớp Scanner nhập xuất dữ liệu*/
         Scanner input = new Scanner(System.in);
         System.out.println("Nhập phần tử muốn xóa");
+        /*Quét mảng, nhập phần tử muốn xóa có giá trị = x*/
         int x = input.nextInt();
-        for (int i=0;i<array.length;i++){
-            if(array[i]==x){
-                array[i]=array[i+1];
-                for (int j=i;j<array.length-1;j++){
-                    array[j]=array[j+1];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == x) {
+                array[i] = array[i + 1];
+                for (int j = i; j < array.length - 1; j++) {
+                    array[j] = array[j + 1];
                 }
             }
         }
         System.out.println("Mảng sau khi xóa");
-        for (int i=0;i<array.length;i++){
-            System.out.print(array[i]+ " ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
     }
-    }
+}
