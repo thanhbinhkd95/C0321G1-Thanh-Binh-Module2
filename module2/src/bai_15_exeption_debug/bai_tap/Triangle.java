a91824fa5648271ae1b3f1f60cbc52dd99a8d7a5
unknown <thanhbinhkd95@gmail.com>
2021-05-26 13:46:26 +0700

    bt

--- module2/src/bai_15_exeption_debug/bai_tap/Triangle.java
+++ module2/src/bai_15_exeption_debug/bai_tap/Triangle.java
@@ -1,50 +1,34 @@
 package bai_15_exeption_debug.bai_tap;
+
 import java.util.Scanner;
+
 public class Triangle {
-    public Triangle(double a, double b, double c) {
-    }
+    private double a;
+    double b;
+    double c;
 
-    static void validate(double a, double b, double c) throws IllegalTriangleException {
-        if (a + b > c && b + c > a && c + a > b && a > 0 && b > 0 && c > 0) {
-            System.out.println("Đây là một tam giác");
+    public Triangle(double a , double b , double c) throws IllegalTriangleException {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            throw new IllegalTriangleException("Không phải là tam giác vì có cạnh nhỏ hơn 0");
+        } else if (a + b <= c || b + c <= a || c + a <= b) {
+            throw new IllegalTriangleException("Không phải tam giác vì tổng 2 cạnh nhỏ hơn 1 cạnh");
         } else {
-            throw new IllegalTriangleException("Không phải tam giác");
+            this.a = a;
+            this.b = b;
+            this.c = c;
+
         }
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            try {
-                System.out.print("Nhập cạnh a: ");
-                double a = Double.parseDouble(scanner.nextLine());
-                System.out.print("Nhập cạnh b: ");
-                double b = Double.parseDouble(scanner.nextLine());
-                System.out.print("Nhập cạnh c: ");
-                double c = Double.parseDouble(scanner.nextLine());
-                Triangle triangle;
-                triangle = getTriangle(a, b, c);
-                validate(a, b, c);
-                System.out.println(triangle.toString());
-                break;
-            } catch (IllegalTriangleException e) {
-                System.out.println(e + " Nhập lại!");
-            } catch (NumberFormatException e) {
-                System.out.println("Nhập lại kí tự số");
-            } catch (Exception e) {
-                System.out.println("Lỗi rồi, nhập lại");
-            } finally {
-                System.out.println("finally");
-            }
-        }
-
-
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "a=" + a +
+                ", b=" + b +
+                ", c=" + c +
+                '}';
     }
-
-    private static Triangle getTriangle(double a, double b, double c) {
-        Triangle triangle;
-        triangle = new Triangle(a, b, c);
-        return triangle;
-    }
-
 }
+
+
+
